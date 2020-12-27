87ddd3b12e65fa894cc461527ff877321e8b3e5f
Егор Тютюкин <egor1109_45@mail.ru>
2020-12-27 13:03:19 +0300

    всякая хуйня

--- Parikmaher/MPI_Parikmakher/Source.cpp
+++ Parikmaher/MPI_Parikmakher/Source.cpp
@@ -1,44 +1,35 @@
 #include <mpi.h>
 #include <stdio.h>
+#include <iostream>
 #include <time.h>
-#include <cstdlib>
-int rank;
-const int lmax = 15; // Количество итераций
 
+int rank;
 void render_vous(int id, int l)
 {
 	FILE* f;
-	char str[256];
+	int msgtag = 15;
 	MPI_Status status;
-	int msgtag = 35;
-	int message;
-
-	if(!rank ) //парикмахер
+	char name[256];
+	if (!rank) //парикмахер
 	{
-		sprintf_s(str, "Barber.txt");
-		fopen_s(&f, str, "w");
+		int message;
+		sprintf_s(name, "report.txt");
+		fopen_s(&f, name, "a+");
 		MPI_Recv(&message, 1, MPI_INT, id, msgtag, MPI_COMM_WORLD, &status);
-		fprintf_s(f, "L = %d, Message = %d\n", l, message);
+		fprintf_s(f, "Принятое сообщение %d Клиент %d На итерации %d\n", message, id, l);
 		fclose(f);
 	}
 	else if (rank) //клиент
 	{
-		sprintf_s(str, "customer%d.txt", id);
-		fopen_s(&f, str, "w");
-		message = id;
+		int message = id;
 		MPI_Send(&message, 1, MPI_INT, 0, msgtag, MPI_COMM_WORLD);
-		fprintf_s(f, "Customer L = %d, Message = %d\n", l, message);
-		fclose(f);
 	}
-	
 }
 
-int main(int argc, char **argv)
+int main(int argc, char** argv)
 {
-	int size;
+	int size, msgtag = 12;
 	MPI_Status status;
-
-
 	if (MPI_Init(&argc, &argv) != MPI_SUCCESS) return 1;
 	if (MPI_Comm_size(MPI_COMM_WORLD, &size) != MPI_SUCCESS)
 	{
@@ -50,56 +41,60 @@ int main(int argc, char **argv)
 		MPI_Finalize();
 		return 3;
 	}
+	//шапочка
 
-	const int msgtag = 35;
-	const int n = 3; // Размер очереди
-	int count=0;
-	int q[n] = {-1, -1 , -1}; // Очередь
-	int pr ;
-	int buf;
+	const int n = 3;
+	int lmax = 10; int pr;
+	int q[n] = { -1,-1,-1 }; //очередь
 
-		for(int l=0; l<lmax; l++)
+	for (int l = 0; l < lmax; l++)
 	{
-		if (!rank)// 0
+		if (!rank) //парикмахер
 		{
-			for(int i=1; i<size; i++)
+			int buf;
+			int count = 0;
+			for (int i = 1; i < size; i++)
 			{
-				MPI_Recv(&buf,1,MPI_INT,MPI_ANY_SOURCE,msgtag,MPI_COMM_WORLD,&status);
-				if((buf==1)&&(count<n))
+				MPI_Recv(&buf, 1, MPI_INT, MPI_ANY_SOURCE, msgtag, MPI_COMM_WORLD, &status);
+				if ((buf == 1) && (count < n))
 				{
-					q[count]=status.MPI_SOURCE;
+					q[count] = status.MPI_SOURCE; //записываем в очередь
 					count++;
 				}
 				else
 				{
-					pr=0;
-					MPI_Send(&pr,1,MPI_INT,status.MPI_SOURCE,msgtag,MPI_COMM_WORLD);
+					pr = 0;
+					MPI_Send(&pr, 1, MPI_INT, status.MPI_SOURCE, msgtag, MPI_COMM_WORLD);
 				}
 			}
-			for(int i=1; i<size; i++)
+			for (int i = 0; i < n; i++)
 			{
-				if(q[i]!=-1)
+				if (q[i] != -1) //если очередь не пустая, то признак заставляет парикхмахера работать
 				{
-					pr=1;
-					MPI_Send(&pr,1,MPI_INT,q[i],msgtag,MPI_COMM_WORLD);
-					render_vous(q[i],l);
+					pr = 1;
+					MPI_Send(&pr, 1, MPI_INT, q[i], msgtag, MPI_COMM_WORLD);
+					render_vous(q[i], l);
 				}
 			}
-			for(int i=0;i<n;i++)
-				q[i]=-1;
-		//	MPI_Barrier(MPI_COMM_WORLD);
+			for (int i = 0; i < n; i++) //обнуляем очередь перед каждой итерацией
+				q[i] = -1;
+
 		}
-		if(rank)
+
+		if (rank)
 		{
 			srand(time(NULL));
-			int zapr=rand()%2;
-			MPI_Send(&zapr,1,MPI_INT,0,msgtag,MPI_COMM_WORLD);
-			MPI_Recv(&pr,1,MPI_INT,0,msgtag,MPI_COMM_WORLD,&status);
-			if(pr==1)
-				render_vous(rank,l);
-	//MPI_Barrier(MPI_COMM_WORLD);
+			int zapr = rand() % 2;
+			MPI_Send(&zapr, 1, MPI_INT, 0, msgtag, MPI_COMM_WORLD);
+			MPI_Recv(&pr, 1, MPI_INT, 0, msgtag, MPI_COMM_WORLD, &status);
+			if (pr == 1)
+			{
+				render_vous(rank, l);
+			}
 		}
-	MPI_Barrier(MPI_COMM_WORLD);
+
+		MPI_Barrier(MPI_COMM_WORLD);
+	}
 	MPI_Finalize();
 	return 0;
 }
